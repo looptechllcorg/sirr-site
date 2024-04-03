b13f8891920b8a36aee858070cf3dbc509ab0a33
Firuznm <firuznmmammadov@gmail.com>
2024-04-03 11:16:48 +0400

    footer media page delete

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -6,15 +6,15 @@ import FooterLogo from "../../assets/logo/footerLogo.svg";
 import FooterInstagramIcon from "../../assets/icons/FooterInstagramIcon";
 import FooterFacebookIcon from "../../assets/icons/FooterFacebookIcon";
 import FooterWhatsappIcon from "../../assets/icons/FooterWhatsappIcon";
-import looptechLogo from "../../assets/logo/looptech-logo-animated.svg"
+import looptechLogo from "../../assets/logo/looptech-logo-animated.svg";
 
 export default function Footer() {
     return (
         <section id={style.FooterSection}>
-            <div style={{padding:0}} className="container">
+            <div style={{ padding: 0 }} className="container">
                 <div className={style.FooterWrapper}>
                     <div className={style.LogoSocial}>
-                        <a  className={style.FooterLogo} href="/">
+                        <a className={style.FooterLogo} href="/">
                             <img src={FooterLogo} alt="burada footer logo var" />
                         </a>
                         <div className={style.footerSocila}>
@@ -46,14 +46,7 @@ export default function Footer() {
                         >
                             Product
                         </NavLink>
-                        <NavLink
-                            to={"/media"}
-                            style={({ isActive }) => {
-                                return isActive ? { color: "rgba(230, 168, 76, 1)" } : {};
-                            }}
-                        >
-                            Media
-                        </NavLink>
+
                         <NavLink
                             to={"/contact"}
                             style={({ isActive }) => {
@@ -66,10 +59,11 @@ export default function Footer() {
                 </div>
                 <hr className={style.line} />
                 <a className={style.looptech} target="_blank" rel="noreferrer" href="https://www.looptech.az/">
-	       	<span className={style.title}>Created by</span>	
-		      <object className={style.looptechLogo} data={looptechLogo} alt="burada LoopTech logo var" />
-	        	</a>
+                    <span className={style.title}>Created by</span>
+                    <object className={style.looptechLogo} data={looptechLogo} alt="burada LoopTech logo var" />
+                </a>
             </div>
         </section>
-    );s
+    );
+    s;
 }
