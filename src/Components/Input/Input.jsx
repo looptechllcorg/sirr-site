13f34ecc8403b822af150a431be2459a38267fd4
Firuznm <firuznmmammadov@gmail.com>
2024-04-07 17:11:00 +0400

    contact form group input border-radius:0

--- src/Components/Input/Input.jsx
+++ src/Components/Input/Input.jsx
@@ -7,5 +7,5 @@ export default function Input({type,placeholder,InpValue,inpName,InpOnChange,inp
 		<input value={InpValue} name={inpName} onChange={InpOnChange} className={style.input} type={type}  placeholder={placeholder} />
      <div className={style.InpError}>{inputError? inputError : null}</div>
     </>
-      )
+      )   
 }
