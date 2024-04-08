7ab0830733289bd42ed844112740b4184ed31eb3
Firuznm <firuznmmammadov@gmail.com>
2024-04-08 19:00:03 +0400

    mainbgImage text close

--- src/Components/MainBgImage/MainBgImage.jsx
+++ src/Components/MainBgImage/MainBgImage.jsx
@@ -6,7 +6,7 @@ export default function MainBgImage({ bgImg, bgImgOnText, bgImgHeight }) {
     return (
         <div style={{ backgroundImage: `url(${sirrSite.baseUrlImage}${bgImg})`, height: bgImgHeight }} className={style.MainBgImageWarapper}>
             <div className={style.overlay}>
-                <h3 className={style.bgOnTitle}>{bgImgOnText}</h3>
+                {/* <h3 className={style.bgOnTitle}>{bgImgOnText}</h3> */}
             </div>
         </div>
     );
